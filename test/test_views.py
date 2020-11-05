fb9ea29fa468e76281907c66b1e9e3fea3955b2f
vdi-student <vdi-student@EDU-WSB.local>
2020-11-05 12:59:22 +0100

    lint4

--- test/test_views.py
+++ test/test_views.py
@@ -16,4 +16,4 @@ class FlaskrTestCase(unittest.TestCase):
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
         self.assertEqual(b'{ "imie":"Emka", "mgs":"Hello World!"}',
-                            rv.data)
+                         rv.data)
